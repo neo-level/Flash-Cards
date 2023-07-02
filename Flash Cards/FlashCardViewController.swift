6cff077019e0325b88d672f0ce8b192e4255de28
neo-level <frederik@neo-level.com>
2023-07-02 12:00:50 +0200

    start with the view, created the function to make a flashcard when the submit button is clicked.

--- Flash Cards/FlashCardViewController.swift
+++ Flash Cards/FlashCardViewController.swift
@@ -8,22 +8,27 @@
 import UIKit
 
 class FlashCardViewController: UIViewController {
-
+    @IBOutlet var wordTextField: UITextField!
+    @IBOutlet var answerTextField: UITextField!
+    @IBOutlet var submitButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /**
+     Creates a flash card when the submit button is clicked.
+     */
+    @IBAction func createFlashCard(_ sender: UIButton) {
+        guard let word = wordTextField.text, !word.isEmpty,
+              let answer = answerTextField.text, !answer.isEmpty else { return }
+        
+        let flashcard = FlashCard(word: word, answer: answer)
+        DataManager.shared.addFlashCard(flashcard)
+        
+        wordTextField.text = ""
+        answerTextField.text = ""
     }
-    */
-
 }
