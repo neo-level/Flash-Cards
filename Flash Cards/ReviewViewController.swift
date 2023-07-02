e0fda3fd97d105053e06ebfdd54d5ca1f70fc317
neo-level <frederik@neo-level.com>
2023-07-02 12:14:39 +0200

    fleshed out the review view. created actions to start a review, flip a card and handle according to a correct or incorrect answer.

--- Flash Cards/ReviewViewController.swift
+++ Flash Cards/ReviewViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class ReviewViewController: UIViewController {
+    @IBOutlet var cardLabel: UILabel!
+    @IBOutlet var flipButton: UIButton!
+    @IBOutlet var correctButton: UIButton!
+    @IBOutlet var wrongflipButton: UIButton!
+    
+    private var currentFlashCard: FlashCard?
+    private var isShowingAnswer = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,15 +22,39 @@ class ReviewViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /**
+     Begins with the review process.
+     */
+    @IBAction func startReview(_ sender: UIButton) {
+        currentFlashCard = DataManager.shared.getRandomFlashCard()
+        guard let flashcard = currentFlashCard else { return }
+        
+        cardLabel.text = flashcard.word
+        isShowingAnswer = false
     }
-    */
-
+    
+    /**
+     Flips the card showing the answer.
+     */
+    @IBAction func flipCard(_ sender: UIButton) {
+        guard let flashcard = currentFlashCard else { return }
+        
+        cardLabel.text = isShowingAnswer ? flashcard.word : flashcard.answer
+        isShowingAnswer.toggle()
+    }
+    
+    @IBAction func answerWasCorrect(_ sender: UIButton ) {
+        guard let flashcard = currentFlashCard else { return }
+        
+        DataManager.shared.updateFlashCardMastery(flashcard, wasCorrect: true)
+        startReview(sender)
+    }
+    
+    @IBAction func answerWasWrong(_ sender: UIButton ) {
+        guard let flashcard = currentFlashCard else { return }
+        
+        DataManager.shared.updateFlashCardMastery(flashcard, wasCorrect: false)
+        startReview(sender)
+    }
+    
 }
