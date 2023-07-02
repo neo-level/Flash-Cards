d1fecd95e9fadc9a159036a246f54a0d96ea613f
neo-level <frederik@neo-level.com>
2023-07-02 11:05:00 +0200

    created the corresponding views for the button in the main view. directing them to the correct view when the button is pressed.

---
+++ Flash Cards/FlashCardViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FlashCardViewController.swift
+//  Flash Cards
+//
+//  Created by Frederik Desmet on 02/07/2023.
+//
+
+import UIKit
+
+class FlashCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
